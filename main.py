9ba818716ec005f5f6f83c6856fe8ee119f505ca
nizan <nizanm91@gmail.com>
2021-05-13 19:01:54 +0300

    init

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import hashlib
 
+
+class MerkelTreeA:
+    def __init__(self, data):
+        self.leftNode = None
+        self.rightNode = None
+        self.data = data
+        self.hashedData = hashlib.sha256(data)
+
+
+def addNode(data):
+    return
+
+
 if __name__ == '__main__':
     while True:
         usrInput = input()
         usrInputParsed = usrInput.split(" ")
         if usrInputParsed[0] == "1":
+            addNode(usrInputParsed[1])
             print("invalid input!")
             continue
         x = 2
